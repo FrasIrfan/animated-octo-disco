f9439ff08ce8a31ac34c86073e0901608f42be17
FrasIrfan <leofras001@gmail.com>
2022-03-13 01:40:28 +0500

    Dynamic program for all numbers

---
+++ decimalToBinaryDynamic.cpp
@@ -0,0 +1,42 @@
+    #include <iostream>
+    using namespace std;
+    void decimalToBinary(){
+
+int n,i,rem;
+//string binary ;
+    cout<<"Decimal to Binary Conversion"<<endl;
+    cout<<"Enter Number"<<endl;
+    cin>>n; //9
+    //binary = ""; //empty value
+
+    for(i=0;i<100;i++)
+{
+    rem=n%2; //modulus 9%2=1   //4%2 = 0 //2%2 = 0
+     n=n/2; //quotient 9/2=4  //4=2 = 2  //2/2 =1
+
+     
+     cout<<"rem = "<<rem<<endl; //rem = 1
+
+     if (n == 1){ //4 false //2 false //1 true
+         break;
+     } 
+     else if (n == 0){ //4 false 2 false
+         break; 
+     }
+    
+}
+    cout <<n<<endl;
+
+    //reverse(binary.begin(),binary.end()); //reverse of a string
+    //cout<<binary<<endl; 
+
+
+    }
+    int main()
+
+{
+
+    decimalToBinary();
+
+    return 0;
+}
