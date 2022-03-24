cfd883e157b2746605752185f58b170709d818fa
FrasIrfan <leofras001@gmail.com>
2022-03-24 19:12:25 +0500

    For the number 83

--- decimalToBinaryfor83.cpp
+++ decimalToBinaryfor83.cpp
@@ -9,26 +9,26 @@ int n,n1,n2,n3,n4,n5,rem1,rem2,rem3,rem4,rem5,rem6;
     cout<<"Enter Number"<<endl;
     cin>>n;
 
-    rem1=n%2;   //1
-n1=n/2; //41
-    rem2=n1%2;  //1
-n2=n1/2;    //20
-    rem3=n2%2;  //0
-n3=n2/2;    //10
-    rem4=n3%2;  //0
-n4=n3/2;    //5
-    rem5=n4%2;  //1
-n5=n4/2;    //2
-    rem6=n4%2;  //1
+        rem1=n%2;   //1
+    n1=n/2; //41
+        rem2=n1%2;  //1
+    n2=n1/2;    //20
+        rem3=n2%2;  //0
+    n3=n2/2;    //10
+        rem4=n3%2;  //0
+    n4=n3/2;    //5
+        rem5=n4%2;  //1
+    n5=n4/2;    //2
+        rem6=n4%2;  //1
 
-    cout<<"Binary = "<<rem1<<rem2<<rem3<<rem4<<rem5<<rem6<<endl;
+        cout<<"Binary = "<<rem1<<rem2<<rem3<<rem4<<rem5<<rem6<<endl;
 
     }
     int main()
 
-{
+    {   
 
     decimalToBinary();
-
     return 0;
-}
+    
+    }
